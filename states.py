3e2a86fba3b4d76cc56fa9a5e861ce99f37e28ba
Umar221-gd <umarzairov568@gmail.com>
2025-02-25 20:01:27 +0600

    hw5

--- states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class ProductState(StatesGroup):
-    name = State()
-    category = State()
-    photo = State()
-    size = State()
-    price = State()
